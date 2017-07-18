1e69dbc3a196480132a47872be7358afedabbf63
fountain111 <fountainoy@icloud.com>
2017-07-18 23:13:06 +0800

    test1 2
    12

--- test1.py
+++ test1.py
@@ -1,61 +1,10 @@
-import numpy as np
+import glob as gl
 import pandas as pd
-exampleSize = 70
-
-startPosition = 0
-churnFlagPosition = 60
-
-
-reader = pd.read_csv('chunk 0.csv')
-'''reader = pd.read_csv('201705_sort.txt', sep=',',
-                     names= ["month", "id", "code", "1","2","3","4","5","6","7","8","9","10",
-                          "11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"],chunksize=7000000)
-'''
-
-
-reader = reader.drop(['month','id','code'])
-#reader = pd.read_csv('201705_sort.txt',sep=',',chunksize=700)
-chunks = []
-chunkDf = pd.DataFrame()
-i =0
-files = 'chunk {chunkOrder}.csv'
-for chunk in reader:
-
-    chunks.append(chunk)
-    chunkDf = pd.concat(chunks, ignore_index=True)
-    chunkDf.to_csv(files.format(chunkOrder = i),index=False)
-    i = i+1
-    chunks = []
-def test():
-
-    chunkSize = 7000
-    notChurnDf = pd.DataFrame()
-    churnDf = pd.DataFrame
-    loop = True
-    notChurnSamples = []
-    churnSamples = []
-    while(loop):
-        startPosition = 0
-        try:
-            chunk = reader.get_chunk(chunkSize)
-        except StopIteration:
-
-            loop = False
-
-        while(startPosition<chunkSize):
-            if(startPosition == chunkSize):
-                print()
-            sample = chunk.iloc[startPosition:(startPosition + exampleSize), :]
-            if (sample.iloc[60,:]['1'] == '1'):
-                churnSamples.append(sample)
-                churnDf = pd.concat(churnSamples, ignore_index=True)
-                print('离网')
-            if (sample.iloc[60,:]['1'] == '0'):
-                notChurnSamples.append(sample)
-                notChurnDf= pd.concat(notChurnSamples, ignore_index=True)
-
-            startPosition = startPosition+exampleSize
-    notChurnDf.to_csv('notChurn.csv', index=False)
-    churnDf.to_csv('churn.csv', index=False)
-
-
+path = r'D:\samples\201704\churn'
+allFiles = gl.glob(path +"\*.csv")
+fileList = []
+for files in allFiles:
+    reader = pd.read_csv(files)
+    fileList.append(reader)
+df = pd.concat(fileList,ignore_index=True)
+df.to_csv(path+'\churn01.csv',index=False)
--- test2.py
+++ test2.py
@@ -9,39 +9,48 @@ churnFlagPosition = 60
 churnSamples = []
 notChurnSamples = []
 notChurnDf = pd.DataFrame()
-reader = pd.read_csv('chunk 0.csv')
-
-reader = pd.read_csv('chunk 14.csv')
-reader_churnflg = reader[reader['code']=='churn_flg']
-
-churn = reader_churnflg[reader_churnflg['1'] == '1']
-chunkSize = 16
 
 
 
 
-def splitChurnAndWrite(files):
+
+def splitChurnAndWrite():
+    chunkSize = 15
     for chunkNumber in range(chunkSize):
         files = 'chunk {chunkOrder}.csv'
         reader = pd.read_csv(files.format(chunkOrder=chunkNumber))
+        reader_churnflg = reader[reader['code'] == 'churn_flg']
+        churn = reader_churnflg[reader_churnflg['1'] == '1']
         churnSamples = []
         for i in churn.index:
             startPosition = i-60
             churnSample = reader.loc[startPosition:startPosition+exampleSize-1,:]
             churnSamples.append(churnSample)
-
-        churnSamples = pd.concat(churnSamples, ignore_index=True)
-        churnSamples.to_csv(files+'churn.csv',index=False)
+        churnSamplesDf = pd.concat(churnSamples, ignore_index=True)
+        filesW = 'chunk {chunkOrder}'
+        churnSamplesDf.to_csv(filesW.format(chunkOrder=chunkNumber)+' churn.csv',index=False)
+        churnSamples = 0
+        churnSamplesDf = 0
 
 def splitNotChurnAndWrite():
-    notChurnBool = reader['month'] == 201706
-    for i in churn.index:
-        startPosition = i - 60
-        notChurnBool[startPosition:startPosition + exampleSize] = False
-
+    chunkSize = 15
+    for chunkNumber in range(chunkSize):
+        files = 'chunk {chunkOrder}.csv'
+        reader = pd.read_csv(files.format(chunkOrder=chunkNumber))
+        reader_churnflg = reader[reader['code'] == 'churn_flg']
+        churn = reader_churnflg[reader_churnflg['1'] == '1']
+        notChurnBool = reader['month'] == 201701
+        for i in churn.index:
+            startPosition = i - 60
+            notChurnBool[startPosition:startPosition + exampleSize] = False
+        filesW = 'chunk {chunkOrder}'
+        newReader = reader[notChurnBool]
+        newReader.to_csv(filesW.format(chunkOrder=chunkNumber)+' notChurn.csv',index=False)
+        newReader = 0
+        reader = 0
 
 def splitFile():
-    reader = pd.read_csv('YBK201704_SORT.txt', sep=',',
+    reader = pd.read_csv('YBK201701_SORT.txt', sep=',',
                          names= ["month", "id", "code", "1","2","3","4","5","6","7","8","9","10",
                               "11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"],chunksize=7000000)
     chunks = []
@@ -54,3 +63,7 @@ def splitFile():
         chunkDf.to_csv(files.format(chunkOrder=i), index=False)
         i = i + 1
         chunks = []
+
+#splitChurnAndWrite()
+#splitFile()
+splitNotChurnAndWrite()
